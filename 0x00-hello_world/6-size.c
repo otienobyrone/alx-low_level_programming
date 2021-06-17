3fa4f34ac6190cf8d532d74dd1147c70c0bd94c9
Byrone Paul <otienobyrone68@gmail.com>
2021-06-17 21:46:21 +0300

    data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@ int longType;
 int longlongType;
 float floatType;
 // sizeof evaluates the size of a variable
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long: %ld bytes\n", sizeof(longType));
-printf("Size of long long: %lld bytes\n", sizeof(intType));
-printf("Size of float is %ld bytes\n",sizeof(floatType));
-return 0;
-}
+  printf("Size of char: %zu byte\n", sizeof(charType));
+  printf("Size of int: %zu bytes\n", sizeof(intType));
+  printf("Size of long init: %ld bytes\n", sizeof(longType));
+  printf("Size of long long init: %ld bytes\n", sizeof(intType));
+  printf("Size of float is %ld bytes\n",sizeof(floatType));
+  return 0;
+    }
