d64f9ed2e02540ae70ed78a2f0d7f074e6f13c84
Byrone Paul <otienobyrone68@gmail.com>
2021-06-21 12:01:12 +0300

    print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+continue;
+}
+putchar('\n');
+return (0);
+}
