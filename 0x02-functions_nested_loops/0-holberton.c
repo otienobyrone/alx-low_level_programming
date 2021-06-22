7cb67344b1b8b194c11254a6fdc4bbf0f1fd1fc9
Byrone Paul <otienobyrone68@gmail.com>
2021-06-22 14:29:05 +0300

    first commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
+/**
+ * main- main.block
+ *
+ * Return:Always 0 (Success)
+ */
 
-int main () {
-   int Holberton;
+int main (void) 
+{
 
-    {
-      putchar(Holberton);
-   }
-    {
-     putchar('\n');
-    }
-   
-   return(0);
+         _putchar('H');
+         _putchar('o');
+	 _putchar('l');
+	 _putchar('b');
+	 _putchar('e');
+         _putchar('r');
+         _putchar('t');
+         _putchar('o');
+	 _putchar('n');
+	 _putchar('\n');
+	 return(0);
 }
