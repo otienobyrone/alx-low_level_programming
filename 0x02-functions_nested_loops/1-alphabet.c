8ac1db6711402fa2a8011d6895b410e2b437f726
Byrone Paul <otienobyrone68@gmail.com>
2021-06-23 11:31:20 +0300

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * print_alphabet - check the code for Alx school students.
@@ -13,7 +13,7 @@ char c;
 
 for (c = 'a'; c <= 'z'; c++)
 {
-putchar(c);
+_putchar(c);
 }
-putchar('\n');
+_putchar('\n');
 }
