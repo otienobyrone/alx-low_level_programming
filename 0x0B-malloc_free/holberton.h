a33da82883a023b70117f17ff359f542e2708316
Byrone Paul <otienobyrone68@gmail.com>
2021-07-13 15:15:42 +0300

    malloc

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,5 +1,13 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+/**
+ * File: holberton.h
+ * Auth: Byrone Paul
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0A-malloc_free directory.
+ */
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
